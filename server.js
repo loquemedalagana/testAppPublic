dc80d1169af5a731251b4b1468b6a75ccc017567
loquemedalagana <nadamasquetu012@gmail.com>
2020-09-22 13:09:48 +0900

    set dev

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+
+const app = express();
+app.set('port', 8000);
+app.set('view engine', 'html');
+
+nunjucks.configure('views', {
+    express: app,
+    watch: true
+})
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use(cookieParser('MYSECRET'));
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'MYSECRET',
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    }
+}));
+
+app.use(require('./router'));
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
+
+
